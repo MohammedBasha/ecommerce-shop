3998e9970f5817a6c8771707e86622313891147c
MohammedBasha <me@mohammedbasha.com>
2021-07-17 11:55:25 +0200

    Adding the manage members page

--- admin/members.php
+++ admin/members.php
@@ -22,9 +22,63 @@ if (isset($_SESSION['Username'])) {
     // If the page is the manage page
     if ($do == 'manage') { // Manage page
 
-        echo 'Welcome to the manage page ';
-        echo '<a href="members.php?do=add" title="Add new member">Add new member</a>';
+        // Selectd all the users
+        $stmt = $con->prepare("SELECT * FROM users");
+        $stmt->execute(); // execute the sql statement
+        $rows = $stmt->fetchAll(); // get all the records
+        ?>
+        <div class="members-page insert-members inner-content">
+            <div class="container">
+                <div class="row">
+                    <h1 class="col-12 mt-5 text-center">Manage members</h1>
+                    <table class="col-12 table table-bordered mt-5 mb-5">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Full Name</th>
+                            <th scope="col">Registered Date</th>
+                            <th scope="col">Control</th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
+                        <?php
+                            foreach($rows as $row) {
+                        ?>
+                            <tr>
+                                <th scope="row">
+                                    <?php echo $row['UserID'] ?>
+                                </th>
+                                <td>
+                                    <?php echo $row['Username'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['Email'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['FullName'] ?>
+                                </td>
+                                <td>
+                                    <?php  ?>
+                                </td>
+                                <td>
+                                    <a href="members.php?do=edit&userid=<?php echo $row['UserID'] ?>" title="Edit" class="btn btn-success">Edit</a>
+                                    <a href="#" title="Delete" class="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        <?php }; ?>
+                        </tbody>
+                    </table>
+                    <a href="members.php?do=add" title="Add new member" class="btn btn-primary btn-lg">
+                        <i class="fas fa-plus"></i>
+                        <span>Add new member<span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <?php
     } elseif ($do == 'add') { // Add members page
         ?>
         <div class="members-page-inner-content-wrapper add-members inner-content">
