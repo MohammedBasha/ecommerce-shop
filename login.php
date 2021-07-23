c38b8db211315d5b30b999b648c0c826e24a7ce4
MohammedBasha <me@mohammedbasha.com>
2021-07-23 07:08:51 +0200

    validating the inputs coming from signup form

--- login.php
+++ login.php
@@ -18,24 +18,100 @@ include "init.php";
 
 // Checking if the user coming from HTTP post request
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // POST should be uppercase
-    $frontuser = $_POST['username'];
-    $frontpassword = $_POST['password'];
-    $hashedPass = sha1($frontpassword); // hashing the password
 
-    // Checking if the user exists in the database and is an admin (GroupID = 1)
-    $stmt = $con->prepare("SELECT Username, Password
+    if (isset($_POST['login'])) {
+        $frontuser = $_POST['username'];
+        $frontpassword = $_POST['password'];
+        $hashedPass = sha1($frontpassword); // hashing the password
+
+        // Checking if the user exists in the database
+        $stmt = $con->prepare("SELECT Username, Password
                             FROM users
                             WHERE Username = ?
                             AND Password = ?");
-    $stmt->execute([$frontuser, $hashedPass]);
-    $rowCount = $stmt->rowCount();
+        $stmt->execute([$frontuser, $hashedPass]);
+        $rowCount = $stmt->rowCount();
 
-    // Checking if there's a user in the database, then create a session with its username, and redirect him to the dashboard.php
-    if ($rowCount > 0) {
-        $_SESSION['frontuser'] = $frontuser; // Registering session name
+        // Checking if there's a user in the database, then create a session with its username, and redirect him to the dashboard.php
+        if ($rowCount > 0) {
+            $_SESSION['frontuser'] = $frontuser; // Registering session name
 
-        header('Location: index.php');
-        exit();
+            header('Location: index.php');
+            exit();
+        }
+    } else {
+        // Assigning the signp form values to variables
+        $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+        $hashedPass = sha1($password); // hashing the password
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+        // Validate the form
+        $formErrors = [];
+
+        if (empty($username)) {
+            $formErrors[] = '<div class="error-username alert alert-warning alert-dismissible fade show mb-3" role="alert">
+    Username Can\'t be <strong>Empty</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>';
+        }
+
+        if (strlen($username) < 4) {
+            $formErrors[] = '<div class="error-username alert alert-warning alert-dismissible fade show mb-3" role="alert">
+    Username Can\'t be less than <strong>4</strong> characters long
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>';
+        }
+
+        if (strlen($username) > 20) {
+            $formErrors[] = '<div class="error-username alert alert-warning alert-dismissible fade show mb-3" role="alert">
+    Username must be less than <strong>20</strong> characters long
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>';
+        }
+
+        if (empty($password) || empty($password2)) {
+            $formErrors[] = '<div class="error-password alert alert-warning alert-dismissible fade show mb-3" role="alert">
+    Password Can\'t be <strong>Empty</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>';
+        }
+
+        if ($password !== $password2) {
+            $formErrors[] = '<div class="error-password alert alert-warning alert-dismissible fade show mb-3" role="alert">
+    Passwords didn\'t <strong>Match</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>';
+        }
+
+        if (empty($email)) {
+            $formErrors[] = '<div class="error-email alert alert-warning alert-dismissible fade show mb-3" role="alert">
+    Email field can\'t be <strong>empty</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>';
+        }
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) != true) {
+            $formErrors[] = '<div class="error-email alert alert-warning alert-dismissible fade show mb-3" role="alert">
+    Email is not <strong>valid</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>';
+        }
     }
 }
 
@@ -53,7 +129,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // POST should be uppercase
                 <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" name="password">
             </div>
-            <button type="submit" class="btn btn-primary btn-block">Login</button>
+            <button type="submit" name="login" class="btn btn-primary btn-block">Login</button>
         </form>
         <form class="signup-form-front col-12" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="form-group">
@@ -72,8 +148,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // POST should be uppercase
                 <label for="email">Email</label>
                 <input type="email" class="form-control" id="email" name="email">
             </div>
-            <button type="submit" class="btn btn-primary btn-block">Sign up</button>
+            <button type="submit" name="signup" class="btn btn-primary btn-block">Sign up</button>
         </form>
+        <div class="forms-errors col-12 text-center">
+            <?php
+            if (!empty($formErrors)) {
+                foreach($formErrors as $error) {
+                    echo $error;
+                }
+            }
+            ?>
+        </div>
     </div>
 </div>
 
